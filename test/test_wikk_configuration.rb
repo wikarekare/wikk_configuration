35898496c1a945d0c2432fae009e83889489d61b
robertburrowes <r.burrowes@auckland.ac.nz>
2022-06-13 13:51:50 +1200

    rubocop

--- test/test_wikk_configuration.rb
+++ test/test_wikk_configuration.rb
@@ -1,8 +1,8 @@
-require "test/unit"
-require "wikk_configuration"
+require 'test/unit'
+require 'wikk_configuration'
 
 class TestBlah < Test::Unit::TestCase
   def test_sanity
-    flunk "write tests or I will kneecap you"
+    flunk 'write tests or I will kneecap you'
   end
 end
